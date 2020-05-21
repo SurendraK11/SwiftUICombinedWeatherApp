7c6373f596ada8b79d3cffc9ca54d0006c4103e3
surendrak11 <surendrak11@gmail.com>
2020-05-21 18:32:52 +0100

    Initial Commit

---
+++ SwiftUICombinedWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICombinedWeatherApp
+//
+//  Created by Surendra on 21/05/2020.
+//  Copyright © 2020 Surendra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
